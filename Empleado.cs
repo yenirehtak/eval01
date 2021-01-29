f2e07dccb35c3bae1ac137d939d4f8d4b6012cd9
Katherine Castro <katherine132000@hotmail.com>
2021-01-29 16:13:00 -0400

    Agregar archivos de proyecto.

---
+++ Empleado.cs
@@ -0,0 +1,117 @@
+﻿using System;
+namespace Empleados {
+	public class Empleado 
+	{
+        private string nomapell;
+        private string cedula;
+        private int edad;
+        private double salario;
+        //declaracion de los métdoso set y get
+        public string NomApell
+        {
+            get
+            {
+                return nomapell;
+            }
+            set
+            {
+                nomapell = value;
+            }
+        }
+        public string Cedula
+        {
+            get
+            {
+                return cedula;
+            }
+            set
+            {
+                cedula = value;
+            }
+        }
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+            set
+            {
+                edad = value;
+            }
+        }
+        public double Salario
+        {
+            get
+            {
+                return salario;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    salario = 80;
+                }
+                else
+                {
+                    salario = value;
+                }
+            }
+        }
+        public virtual string solicitarJubilarse()
+        {
+            return "El empleado procedera a jubilarse";
+        }
+
+    }
+    public class EmpdeAlmacen : Empleado
+    {
+        private string id;
+        private bool ExistenciaMercancia;
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public bool Existenciamercancia
+        {
+            get
+            {
+                return ExistenciaMercancia;
+            }
+            set
+            {
+                ExistenciaMercancia = value;
+            }
+        }
+        //metodos unicos del empleado del almacen 
+        public override string solicitarJubilarse()
+        {
+            return "El almacenista procedera a jubilarse";
+        }
+        public void RecibirMercancia()
+        {
+            Console.WriteLine("La mercancia ha sido recibida");
+        }
+        public void EnviarMercancia()
+        {
+            Console.WriteLine("La mercancia ha sido enviada");
+        }
+        public void RealizarControl()
+        {
+            Console.WriteLine("Gestionando el control de calidad");
+        }
+        public void RealizarInventario()
+        {
+            Console.WriteLine("Gestionando el inventario del almacen");
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Empleados;
+namespace eval01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char tipoEmpleado;
+            for (int i = 0; i < 5; i++)
+            {
+                char resp;
+                Empleado oEmpleado = new Empleado();
+                Console.WriteLine("Bienvenido. Ingrese su nombre");
+                oEmpleado.NomApell = Console.ReadLine();
+                Console.WriteLine("Ingrese su cedula");
+                oEmpleado.Cedula = Console.ReadLine();
+                Console.WriteLine("Ingrese su edad");
+                oEmpleado.Edad = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese su salario");
+                oEmpleado.Salario = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Por favor ingrese la inicial de su departamento: A(almacen) / V(ventas) / O(obrero)");
+                tipoEmpleado = Convert.ToChar(Console.ReadLine());
+                if (tipoEmpleado == 'A' || tipoEmpleado == 'a')
+                {
+                    EmpdeAlmacen oEmpdeAlmacen = new EmpdeAlmacen();
+                    oEmpdeAlmacen.Existenciamercancia = true;
+                    Console.WriteLine("Ingrese su codigo de identificacion");
+                    oEmpdeAlmacen.Id = Console.ReadLine();
+                    Console.WriteLine("¿Desea recibir la mercancia? S / n");
+                    resp = Convert.ToChar(Console.ReadLine());
+                    if (resp == 'S' || resp == 's')
+                    {
+                        oEmpdeAlmacen.RecibirMercancia();
+                        oEmpdeAlmacen.EnviarMercancia();
+                        oEmpdeAlmacen.RealizarControl();
+                        oEmpdeAlmacen.RealizarInventario();
+                        oEmpdeAlmacen.solicitarJubilarse();
+                        Console.WriteLine("Su nombre es {0}, tiene {1} de edad, su cedula es {2} y su salario es {3}", oEmpleado.NomApell, oEmpleado.Edad, oEmpleado.Cedula, oEmpleado.Salario);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No hay nada para hacer.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No hay informacion sobre su departamento.");
+                }
+            }
+        }
+    }
+}
